793cfe45cc7b5f8866af71dff6fe4556ee762b04
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-05-15 13:21:57 +0200

    changed to case-insensitive sorting of license ids

--- src/get_licenses_spdx.ts
+++ src/get_licenses_spdx.ts
@@ -4,10 +4,13 @@ async function getit() {
     const spdxUrl = "https://raw.githubusercontent.com/spdx/license-list-data/master/json/licenses.json";
     const response = await fetch(spdxUrl);
     const data = await response.json();
-    const licenseIds = data.licenses.map((license: any) => {return license.licenseId as string}).sort();
+    const licenseIds = data.licenses.map((license: any) => {return license.licenseId as string});
+    const sortedLicenseIds = licenseIds.sort((self: string, other: string) => {
+        return self.toLowerCase().localeCompare(other.toLowerCase());
+    });
     const stringified = JSON.stringify({
         "$comment": "SPDX license list; releaseDate=" + data.releaseDate + "; source=" + spdxUrl,
-        "enum": licenseIds,
+        "enum": sortedLicenseIds,
         "type": "string"
     });
     console.log(stringified);
